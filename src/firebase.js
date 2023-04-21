a0f6ab99c7b8f8f55efb0cabc558856f02c59b6b
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2023-04-21 07:57:27 -0700

    initil / HomeScreen / Nav / Banner / Logo

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDGwpoXBvhaeStL_NJ40Bz-3EVfvLC_g4w",
+  authDomain: "netflix-build-9be93.firebaseapp.com",
+  projectId: "netflix-build-9be93",
+  storageBucket: "netflix-build-9be93.appspot.com",
+  messagingSenderId: "881845953005",
+  appId: "1:881845953005:web:5f7507a5288c0197ca47e4"
+};
