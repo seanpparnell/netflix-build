5636bf95337df065d07e9b8154997850f363ae46
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2023-04-25 08:09:08 -0700

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import HomeScreen from './HomeScreen';
+import React from "react";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
